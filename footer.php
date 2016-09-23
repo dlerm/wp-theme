bc9dd4d6a18033194915643399f3c4eb31861607
Daniel <lerman@Daniels-MacBook-Pro.local>
2016-09-22 21:06:52 -0700

    Initial build

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- 
+<div id="footer">
+	<div id="bottom-menu"><?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?></div>
+	<div id="copyright">
+		Please leave this line intact
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">theme design by Daniel Lerman © <?php the_time('Y'); ?></a>
+	</div>
+</div>
+ -->
+<?php wp_footer(); ?>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/mycustomscript.js"></script> 
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Daniel Lerman | Developer</title>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:300,600,400,900,300italic,400italic,700italic,600italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
+  <!-- <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/main.css" type="text/css" media="screen,projection" /> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" type="text/css" media="screen,projection" />
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header id="header">
+    <div id="nav-wrapper">
+      <nav id="main-nav" class="flex">
+        <a href="/" id="header-logo" class="flex" title="Daniel Lerman">
+          <div class="circle"></div>
+          <span class="firstname">D</span>
+          <span class="lastname">L</span>
+        </a>
+      </nav>
+      <div class="social-links">
+        <a class="linkedin" href="https://www.linkedin.com/in/daniel-lerman-2933498b"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        <a class="github" href="https://github.com/dlerm"><i class="fa fa-github" aria-hidden="true"></i></a>
+      </div>
+    </div>
+  </header>
